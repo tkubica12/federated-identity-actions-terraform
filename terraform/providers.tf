c75111292232f07a0ba2c96e37b684adde9ae744
Tomáš Kubica <tkubica12@gmail.com>
2022-10-07 07:34:44 +0200

    cleanup backend config

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -11,9 +11,6 @@ terraform {
     storage_account_name = "tkubicastore"
     container_name       = "tfstate"
     key                  = "federated-identity-actions-terraform.terraform.tfstate"
-   # use_oidc             = true
-   # subscription_id      = "00000000-0000-0000-0000-000000000000"
-   # tenant_id            = "00000000-0000-0000-0000-000000000000"
   }
 }
 
