c3dbe1e4c0bd08f9536929d2afa1a464d07bb3dd
Tomáš Kubica <tkubica12@gmail.com>
2022-10-07 07:32:39 +0200

    add statefile backend

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -5,6 +5,16 @@ terraform {
       version = "~>3"
     }
   }
+
+  backend "azurerm" {
+    resource_group_name  = "base"
+    storage_account_name = "tkubicastore"
+    container_name       = "tfstate"
+    key                  = "federated-identity-actions-terraform.terraform.tfstate"
+   # use_oidc             = true
+   # subscription_id      = "00000000-0000-0000-0000-000000000000"
+   # tenant_id            = "00000000-0000-0000-0000-000000000000"
+  }
 }
 
 provider "azurerm" {
